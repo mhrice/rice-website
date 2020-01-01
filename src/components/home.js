909ae2fa285b49906e6bb43c4366b408d04d62c5
Matthew Rice <mhrice@ucsd.edu>
2019-12-31 19:26:23 -0800

    Init commit

---
+++ src/components/home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Piano from "./piano";
+
+class Home extends Component {
+    render(){
+        return (
+        <div>
+            Home
+            <Piano/>
+        </div>
+        )
+    }
+}
+
+export default Home;
---
+++ src/components/piano.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Piano extends Component {
+    render(){
+        return <div>Piano</div>
+    }
+}
+
+export default Piano;
